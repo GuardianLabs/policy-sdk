e88466563a5402dcf13fc07f438b00a1685b9d8b
Vitali Grabovski <vetal2060@gmail.com>
2024-10-28 12:14:56 +0200

    @guardian-network/policy-contracts: add a dedicated business-hours unit-test

--- packages/policy-contracts/test/business-hours.test.ts
+++ packages/policy-contracts/test/business-hours.test.ts
@@ -1,17 +1,19 @@
 import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers';
 import { time } from '@nomicfoundation/hardhat-toolbox/network-helpers';
-import { ethers } from 'hardhat';
-import { TrustedTimezoneOffsetSource } from './types';
-// import { BusinessHoursValidation, TrustedTimezoneOffsetSource } from './types';
 import { expect } from 'chai';
+import { ethers } from 'hardhat';
+import { SolidityAddressType, SolidityUint24ListType } from './solidity-types';
 import { check } from './test-helpers';
-import { deployBusinessHoursContracts } from './utils';
-import { toSeconds } from './utils/business-hours';
+import { BusinessHoursValidation, TrustedTimezoneOffsetSource } from './types';
+import {
+  deployBusinessHoursContracts,
+  solidityDecodeSingleParam,
+} from './utils';
+import { TimezoneOffset, toSeconds } from './utils/business-hours';
+import { InitParams, MockedExecParams } from './utils/init-exec-arguments';
 
-const deployBusinessHoursInstance = async (...args: any[]): Promise<any> => {};
-
-describe('BusinessHours', () => {
-  const timezoneId: string = 'Europe/Kyiv';
+describe('Business Hours', () => {
+  const TIMEZONE_ID: string = 'Europe/Kyiv';
   const timezoneOffset: number = 2 * 60;
   const timezoneOffsetIsNegative = false;
 
@@ -34,143 +36,220 @@ describe('BusinessHours', () => {
     toSeconds(0), // SATURDAY
   ];
 
-  let arbitrarySigner: SignerWithAddress;
   let adminSigner: SignerWithAddress;
   let trustedSourceSigner: SignerWithAddress;
 
   let trustedTimezoneOffsetSourceInstance: TrustedTimezoneOffsetSource;
-  let businessHoursInstance: any /* BusinessHoursValidation */;
+  let businessHoursInstance: BusinessHoursValidation;
 
   before(async () => {
-    [arbitrarySigner, adminSigner, trustedSourceSigner] =
-      await ethers.getSigners();
+    [adminSigner, trustedSourceSigner] = await ethers.getSigners();
 
-    // deploy bh-related instances
-    const instances = await deployBusinessHoursContracts(adminSigner);
-    trustedTimezoneOffsetSourceInstance = instances.trustedTimezoneSource;
+    const offsetParams: TimezoneOffset = [
+      TIMEZONE_ID,
+      timezoneOffset,
+      timezoneOffsetIsNegative,
+    ];
 
-    // add trusted supplier
-    const addTrustedSourceTx = await trustedTimezoneOffsetSourceInstance
-      .connect(adminSigner)
-      .modifyPermittedCallersList(trustedSourceSigner.address, true);
-
-    await addTrustedSourceTx.wait();
-
-    // provide the offset to the trusted source using authorized supplier
-    const provideOffsetTx = await trustedTimezoneOffsetSourceInstance
-      .connect(adminSigner)
-      .supplyTimezoneOffset(
-        timezoneId,
-        timezoneOffset,
-        timezoneOffsetIsNegative,
-      );
-
-    await provideOffsetTx.wait();
-
-    businessHoursInstance = await deployBusinessHoursInstance(
-      arbitrarySigner,
-      timezoneId,
-      (await trustedTimezoneOffsetSourceInstance.getAddress()).slice(2),
-      openingSeconds,
-      closingSeconds,
-    );
+    ({
+      businessHours: businessHoursInstance,
+      trustedTimezoneSource: trustedTimezoneOffsetSourceInstance,
+    } = await deployBusinessHoursContracts(adminSigner, offsetParams));
   });
 
-  describe('#checkBusinessHours', () => {
-    describe('successful call', () => {
-      it('should be open at 23:59:59 on Monday', async () => {
+  describe('Timezone Oracle: Add trusted supplier', () => {
+    it('should add a trusted offset data supplier ', async () => {
+      const supplier = trustedSourceSigner.address;
+      const tx = await trustedTimezoneOffsetSourceInstance
+        .connect(adminSigner)
+        .modifyPermittedCallersList(supplier, true);
+      await tx.wait();
+
+      const isAuthorized =
+        await trustedTimezoneOffsetSourceInstance.authorizedSupplier(supplier);
+      check(isAuthorized, true);
+    });
+  });
+
+  describe('Business Hours Artifact: Initialization', () => {
+    it('should init Business hours artifact', async () => {
+      const openingSecondsUint24List = SolidityUint24ListType.create([
+        ...openingSeconds,
+      ]);
+      const closingSecondsUint24List = SolidityUint24ListType.create([
+        ...closingSeconds,
+      ]);
+      const trustedTimezoneSourceAddress = SolidityAddressType.create(
+        await trustedTimezoneOffsetSourceInstance.getAddress(),
+      );
+
+      const init = InitParams.create(businessHoursInstance).add(
+        TIMEZONE_ID,
+        trustedTimezoneSourceAddress,
+        openingSecondsUint24List,
+        closingSecondsUint24List,
+      );
+
+      const correctInitTx = await businessHoursInstance.init(init.params);
+      const initTimestamp = await time.latest();
+      await expect(correctInitTx)
+        .to.emit(businessHoursInstance, 'BusinessHoursInited')
+        .withArgs(initTimestamp);
+
+      const incorrectInitTx = businessHoursInstance.init(init.params);
+      await expect(incorrectInitTx).to.be.revertedWith('PD-003');
+    });
+  });
+
+  describe('Business Hours Artifact: Check if business is open', () => {
+    describe('failure', () => {
+      let businessHoursInstance: BusinessHoursValidation;
+      let openingSecondsUint24List: SolidityUint24ListType;
+      let closingSecondsUint24List: SolidityUint24ListType;
+      let trustedTimezoneSourceAddress: SolidityAddressType;
+
+      before(async () => {
+        let trustedTimezoneOffsetSourceInstance: TrustedTimezoneOffsetSource;
+        const offsetParams: TimezoneOffset = [
+          TIMEZONE_ID,
+          timezoneOffset,
+          timezoneOffsetIsNegative,
+        ];
+
+        ({
+          businessHours: businessHoursInstance,
+          trustedTimezoneSource: trustedTimezoneOffsetSourceInstance,
+        } = await deployBusinessHoursContracts(adminSigner, offsetParams));
+
+        openingSecondsUint24List = SolidityUint24ListType.create([
+          ...openingSeconds,
+        ]);
+        closingSecondsUint24List = SolidityUint24ListType.create([
+          ...closingSeconds,
+        ]);
+        trustedTimezoneSourceAddress = SolidityAddressType.create(
+          await trustedTimezoneOffsetSourceInstance.getAddress(),
+        );
+      });
+
+      it('when closing time is earlier than opening time', async () => {
+        openingSecondsUint24List.uintArray[0] = 5;
+        closingSecondsUint24List.uintArray[0] = 4;
+
+        const init = InitParams.create(businessHoursInstance).add(
+          TIMEZONE_ID,
+          trustedTimezoneSourceAddress,
+          openingSecondsUint24List,
+          closingSecondsUint24List,
+        );
+
+        const incorrectInitTx = businessHoursInstance.init(init.params);
+        await expect(incorrectInitTx).to.be.revertedWith('BH-005');
+      });
+
+      it('when opening period during single day last less than 60 seconds', async () => {
+        openingSecondsUint24List.uintArray[0] = 40;
+        closingSecondsUint24List.uintArray[0] = 50;
+
+        const init = InitParams.create(businessHoursInstance).add(
+          TIMEZONE_ID,
+          trustedTimezoneSourceAddress,
+          openingSecondsUint24List,
+          closingSecondsUint24List,
+        );
+
+        const incorrectInitTx = businessHoursInstance.init(init.params);
+        await expect(incorrectInitTx).to.be.revertedWith('BH-006');
+      });
+
+      it('when closing time passed as seconds is higher than 24 * 3600 (one day in seconds)', async () => {
+        openingSecondsUint24List.uintArray[0] = 40;
+        closingSecondsUint24List.uintArray[0] = 86401;
+
+        const init = InitParams.create(businessHoursInstance).add(
+          TIMEZONE_ID,
+          trustedTimezoneSourceAddress,
+          openingSecondsUint24List,
+          closingSecondsUint24List,
+        );
+
+        const incorrectInitTx = businessHoursInstance.init(init.params);
+        await expect(incorrectInitTx).to.be.revertedWith('BH-004');
+      });
+    });
+
+    describe('success', () => {
+      it('when open at 23:59:59 on Monday, 23 January 2040 23:59:59 GMT+02:00 (Kyiv)', async () => {
         // Monday, 23 January 2040 23:59:59 GMT+02:00 (Kyiv)
         const nextTimeStamp = 2210968799;
         await time.increaseTo(nextTimeStamp);
 
-        check(await businessHoursInstance.checkBusinessHours(), true);
+        const encodedResult = await businessHoursInstance.exec(
+          MockedExecParams.make({ argsCount: 0 }).params,
+        );
+        const decodedResult = solidityDecodeSingleParam('bool', encodedResult);
+        check(decodedResult, true);
       });
 
-      it('should be open on Monday at 24 pm due to 24:00:00 --> 23:59:59 transform (last second does not count if 24 pm supplied)', async () => {
+      it(`when open on Monday 23 January 2040 at 24 pm due to 24:00:00 --> 23:59:59 transform
+              (last second does not count if 24 pm supplied)`, async () => {
         // Tuesday, 24 January 2040 00:00:00 GMT+02:00 (Kyiv)
         const nextTimeStamp = 2210968800;
         await time.increaseTo(nextTimeStamp);
 
-        check(await businessHoursInstance.checkBusinessHours(), false);
+        const encodedResult = await businessHoursInstance.exec(
+          MockedExecParams.make({ argsCount: 0 }).params,
+        );
+        const decodedResult = solidityDecodeSingleParam('bool', encodedResult);
+        check(decodedResult, false);
       });
 
-      it('should be closed before the Tuesaday business hours', async () => {
+      it('when closed before the Tuesday, 24 January 2040 08:00:00 GMT+02:00 (Kyiv)', async () => {
         // Tuesday, 24 January 2040 08:00:00 GMT+02:00 (Kyiv)
         const nextTimeStamp = 2210997600;
         await time.increaseTo(nextTimeStamp);
 
-        check(await businessHoursInstance.checkBusinessHours(), false);
+        const encodedResult = await businessHoursInstance.exec(
+          MockedExecParams.make({ argsCount: 0 }).params,
+        );
+        const decodedResult = solidityDecodeSingleParam('bool', encodedResult);
+        check(decodedResult, false);
       });
 
-      it('should be open on the Tuesday business hours', async () => {
+      it('when open on the Tuesday, 24 January 2040 12:00:00 GMT+02:00 (Kyiv)', async () => {
         // Tuesday, 24 January 2040 12:00:00 GMT+02:00 (Kyiv)
         const nextTimeStamp = 2211012000;
         await time.increaseTo(nextTimeStamp);
 
-        check(await businessHoursInstance.checkBusinessHours(), true);
+        const encodedResult = await businessHoursInstance.exec(
+          MockedExecParams.make({ argsCount: 0 }).params,
+        );
+        const decodedResult = solidityDecodeSingleParam('bool', encodedResult);
+        check(decodedResult, true);
       });
 
-      it('should be closed after the Tuesday business hours', async () => {
+      it('when closed after the Tuesday, 24 January 2040 18:03:00 GMT+02:00 (Kyiv)', async () => {
         // Tuesday, 24 January 2040 18:03:00 GMT+02:00 (Kyiv)
         const nextTimeStamp = 2211033780;
         await time.increaseTo(nextTimeStamp);
 
-        check(await businessHoursInstance.checkBusinessHours(), false);
+        const encodedResult = await businessHoursInstance.exec(
+          MockedExecParams.make({ argsCount: 0 }).params,
+        );
+        const decodedResult = solidityDecodeSingleParam('bool', encodedResult);
+        check(decodedResult, false);
       });
 
-      it('should be closed on Saturday', async () => {
+      it('when closed on Saturday, 28 January 2040 14:00:00 GMT+02:00 (Kyiv)', async () => {
         // Saturday, 28 January 2040 14:00:00 GMT+02:00 (Kyiv)
         const nextTimeStamp = 2211364800;
         await time.increaseTo(nextTimeStamp);
 
-        check(await businessHoursInstance.checkBusinessHours(), false);
-      });
-    });
-
-    describe('failure', () => {
-      it('when closing time is earlier than opening time', async () => {
-        openingSeconds[0] = 5;
-        closingSeconds[0] = 4;
-
-        await expect(
-          deployBusinessHoursInstance(
-            arbitrarySigner,
-            timezoneId,
-            (await trustedTimezoneOffsetSourceInstance.getAddress()).slice(2),
-            openingSeconds,
-            closingSeconds,
-          ),
-        ).to.be.revertedWith('BH-4');
-      });
-
-      it('when time passed in hours, not in seconds', async () => {
-        openingSeconds[0] = 40;
-        closingSeconds[0] = 50;
-
-        await expect(
-          deployBusinessHoursInstance(
-            arbitrarySigner,
-            timezoneId,
-            (await trustedTimezoneOffsetSourceInstance.getAddress()).slice(2),
-            openingSeconds,
-            closingSeconds,
-          ),
-        ).to.be.revertedWith('BH-6');
-      });
-
-      it('when closing time passed as seconds is higher than 24 * 3600 (one day in seconds)', async () => {
-        openingSeconds[0] = 40;
-        closingSeconds[0] = 86401;
-
-        await expect(
-          deployBusinessHoursInstance(
-            arbitrarySigner,
-            timezoneId,
-            (await trustedTimezoneOffsetSourceInstance.getAddress()).slice(2),
-            openingSeconds,
-            closingSeconds,
-          ),
-        ).to.be.revertedWith('BH-3');
+        const encodedResult = await businessHoursInstance.exec(
+          MockedExecParams.make({ argsCount: 0 }).params,
+        );
+        const decodedResult = solidityDecodeSingleParam('bool', encodedResult);
+        check(decodedResult, false);
       });
     });
   });
