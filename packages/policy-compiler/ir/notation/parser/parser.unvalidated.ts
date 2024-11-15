a8ec380057fbc49be69c70a8aceb9c1101a58283
Vitali Grabovski <vetal2060@gmail.com>
2024-11-16 01:28:31 +0200

    @guardian-network/policy-compiler: add Parser builder with a support dsl based config as types source + make "parser.unvalidated" helper to prevent codebase duplication + isolate un-duplicate pieces into a dedicated method

--- packages/policy-compiler/ir/notation/parser/parser.unvalidated.ts
+++ packages/policy-compiler/ir/notation/parser/parser.unvalidated.ts
@@ -2,6 +2,7 @@ import { Provider } from 'ethers';
 import { IArbitraryDataArtifact__factory } from '../../../../policy-contracts/src/typechain';
 import { TranspilerOutput } from '../../../dsl';
 import { nodeIdByNotation } from '../../../dsl/transformer';
+import { InstanceConfig } from '../../../dsl/transpiler/state';
 import {
   bytesEncodeArgs,
   DSLTypesToIRTypes,
@@ -14,71 +15,89 @@ import {
 } from '../helpers';
 import { ParsingResult, Type, ValidationMiddlware } from './types';
 
+type Parameter = {
+  value: string;
+  index: number;
+};
+
+type KnownParamsAndInitData = {
+  knownExecParams: Parameter[];
+  initDataEncoded: string;
+};
+
+type RetrieveExecAndInitData = (
+  parameters: Array<Parameter>,
+  initArgs: string[],
+  instanceConfig: InstanceConfig,
+  artifactAddress: string,
+  provider?: Provider,
+) => KnownParamsAndInitData | Promise<KnownParamsAndInitData>;
+
+const getKnownExecDataAndInitDataFromDslTypes = (
+  parameters: Array<Parameter>,
+  initArgs: string[],
+  instanceConfig: InstanceConfig,
+  artifactAddress: string,
+  provider?: Provider,
+): KnownParamsAndInitData => {
+  const execConstTypes = instanceConfig.execArguments
+    .filter((el) => el.constant)
+    .map((el) => el.type);
+  const initTypes = instanceConfig.initArguments.map((el) => el.type);
+
+  const knownExecParams = indexConstants(
+    parameters.filter((arg) => isConstant(arg.value)),
+    execConstTypes.map(DSLTypesToIRTypes),
+  );
+  const initDataEncoded = bytesEncodeArgs(
+    initArgs,
+    initTypes.map(DSLTypesToIRTypes),
+  );
+
+  return {
+    knownExecParams,
+    initDataEncoded,
+  };
+};
+
+const getKnownExecDataAndInitDataFromOnchainTypes = async (
+  parameters: Array<Parameter>,
+  initArgs: string[],
+  instanceConfig: InstanceConfig,
+  artifactAddress: string,
+  provider?: Provider,
+): Promise<KnownParamsAndInitData> => {
+  const instance = IArbitraryDataArtifact__factory.connect(
+    artifactAddress,
+    provider!,
+  );
+
+  const { argsTypes } = await instance.getExecDescriptor();
+
+  const { argsTypes: initArgsTypes } = await instance.getInitDescriptor();
+
+  const knownExecParams = indexConstants(
+    parameters.filter((arg) => isConstant(arg.value)),
+    <Type[]>argsTypes,
+  );
+  const initDataEncoded = bytesEncodeArgs(initArgs, <Type[]>initArgsTypes);
+
+  return {
+    knownExecParams,
+    initDataEncoded,
+  };
+};
+
 // note: types dsl-inferred-first
 export const parseIRByDSLTypesWithInterceptor = async (
   { ir, typings }: TranspilerOutput,
   middleware?: ValidationMiddlware,
 ) => {
-  const res: ParsingResult[] = [];
-
-  const artifacts = ir.trim().split(/\r?\n/);
-  for (let [index, artifact] of artifacts.entries()) {
-    artifact = artifact.trim();
-    const { addressClause, paramsClause, initClause } =
-      extractComponents(artifact);
-
-    const artifactAddress = addressClause;
-    const parameters = extractArguments(paramsClause).map((value, index) => ({
-      value,
-      index,
-    }));
-
-    const initArgs = extractArguments(initClause);
-
-    const variables = parameters
-      .filter((val) => strIsVar(val.value))
-      .map((val) => val.index);
-    const substitutions = parameters
-      .filter((val) => strIsSubst(val.value))
-      .map((val) => ({
-        value: val.value.replace(/\|/g, ''),
-        index: val.index,
-      }));
-
-    const currentInstanceConfig = typings[index];
-
-    if (middleware)
-      await middleware.innerValidations(artifactAddress, currentInstanceConfig);
-
-    const execConstTypes = currentInstanceConfig.execArguments
-      .filter((el) => el.constant)
-      .map((el) => el.type);
-    const initTypes = currentInstanceConfig.initArguments.map((el) => el.type);
-
-    const partialExecData = indexConstants(
-      parameters.filter((arg) => isConstant(arg.value)),
-      execConstTypes.map(DSLTypesToIRTypes),
-    );
-    const initData = bytesEncodeArgs(
-      initArgs,
-      initTypes.map(DSLTypesToIRTypes),
-    );
-
-    res.push({
-      id: nodeIdByNotation(artifact, index),
-      artifactAddress,
-      partialExecData,
-      variables,
-      argsCount: parameters.length,
-      substitutions,
-      initData,
-      needsInitialization: initArgs.length != 0,
-    });
-  }
-
-  if (middleware) await middleware.outerValidations(res);
-
-  return res;
+  return parse(
+    { ir, typings },
+    getKnownExecDataAndInitDataFromDslTypes,
+    middleware,
+  );
 };
 
 // note: types onchain-inferred-first
@@ -87,9 +106,23 @@ export const parseIRByOnchainTypesWithInterceptor = async (
   provider: Provider,
   middleware?: ValidationMiddlware,
 ) => {
+  return parse(
+    { ir, typings },
+    getKnownExecDataAndInitDataFromOnchainTypes,
+    middleware,
+    provider,
+  );
+};
+
+const parse = async (
+  transpileOutput: Pick<TranspilerOutput, 'ir' | 'typings'>,
+  exectAndInitData: RetrieveExecAndInitData,
+  middleware?: ValidationMiddlware,
+  provider?: Provider,
+) => {
   const res: ParsingResult[] = [];
 
-  const artifacts = ir.trim().split(/\r?\n/);
+  const artifacts = transpileOutput.ir.trim().split(/\r?\n/);
   for (let [index, artifact] of artifacts.entries()) {
     artifact = artifact.trim();
     const { addressClause, paramsClause, initClause } =
@@ -113,25 +146,19 @@ export const parseIRByOnchainTypesWithInterceptor = async (
         index: val.index,
       }));
 
-    const currentInstanceConfig = typings[index];
+    const currentInstanceConfig = transpileOutput.typings[index];
 
     if (middleware)
       await middleware.innerValidations(artifactAddress, currentInstanceConfig);
 
-    const instance = IArbitraryDataArtifact__factory.connect(
-      artifactAddress,
-      provider,
-    );
-
-    const { argsTypes } = await instance.getExecDescriptor();
-
-    const { argsTypes: initArgsTypes } = await instance.getInitDescriptor();
-
-    const partialExecData = indexConstants(
-      parameters.filter((arg) => isConstant(arg.value)),
-      <Type[]>argsTypes,
-    );
-    const initData = bytesEncodeArgs(initArgs, <Type[]>initArgsTypes);
+    const { knownExecParams: partialExecData, initDataEncoded: initData } =
+      await exectAndInitData(
+        parameters,
+        initArgs,
+        currentInstanceConfig,
+        artifactAddress,
+        provider,
+      );
 
     res.push({
       id: nodeIdByNotation(artifact, index),
