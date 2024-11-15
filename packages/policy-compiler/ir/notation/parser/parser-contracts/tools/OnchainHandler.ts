9e70fa609965630ec10f9ffc71dfa04a1769a1c6
Vitali Grabovski <vetal2060@gmail.com>
2024-11-15 13:08:08 +0200

    @guardian-network/policy-compiler: refactoring (adjust exports + add 'simplified OnchainHandler')

--- packages/policy-compiler/ir/notation/parser/parser-contracts/tools/OnchainHandler.ts
+++ packages/policy-compiler/ir/notation/parser/parser-contracts/tools/OnchainHandler.ts
@@ -10,25 +10,6 @@ export class OnchainHandler implements IOnchainHandler {
 
   constructor(private provider: ContractRunner) {}
 
-  private getExecDescriptorValue = async (instanceAddress: string) => {
-    this.connectInstance(instanceAddress);
-    return this.artifactInstance.getExecDescriptor();
-  };
-
-  private getInitDescriptorValue = async (instanceAddress: string) => {
-    this.connectInstance(instanceAddress);
-    return this.artifactInstance.getInitDescriptor();
-  };
-
-  private getDescriptorsValues = async (instanceAddress: string) => {
-    const execDescriptorValue =
-      await this.getExecDescriptorValue(instanceAddress);
-    const initDescriptorValue =
-      await this.getInitDescriptorValue(instanceAddress);
-
-    return { execDescriptorValue, initDescriptorValue };
-  };
-
   protected connectInstance = (artifactAddress: string) => {
     this.artifactInstance = IArbitraryDataArtifact__factory.connect(
       artifactAddress,
@@ -36,6 +17,16 @@ export class OnchainHandler implements IOnchainHandler {
     );
 
     return this;
+  };
+
+  private getDescriptorsValues = async (instanceAddress: string) => {
+    this.connectInstance(instanceAddress);
+
+    const execDescriptorValue = await this.artifactInstance.getExecDescriptor();
+
+    const initDescriptorValue = await this.artifactInstance.getInitDescriptor();
+
+    return { execDescriptorValue, initDescriptorValue };
   };
 
   getDescriptors = async (instanceAddress: string): Promise<Descriptors> => {
