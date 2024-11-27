ddcb2286daf3d7234f8127dc2abf4bf5bb4eef15
Vitali Grabovski <vetal2060@gmail.com>
2024-11-27 18:19:22 +0200

    @guardian-network/policy-compiler: split tests to onchain, and offchain part + configure hardhat and mocha respectivelly as test runners

--- packages/policy-compiler/test/compiler-onchain-types.test.ts
+++ packages/policy-compiler/test/compiler-onchain-types.test.ts
@@ -1,16 +1,22 @@
 import { expect } from 'chai';
+import { ethers } from 'hardhat';
 import { LacLangCompiler } from '../src';
 import { SNAPSHOTS_PATH, SOURCES_PATH } from './constants';
 import { importJson, toLacSourcePath, toSnapshotPath } from './helpers';
 import { Stub } from './sources';
 
-describe('The "*.lac" code compilation with dsl types check', () => {
+describe('The "*.lac" code compilation with onchain types check', () => {
   const stubCode = Stub.DUMMY_VALID;
 
   describe("Valid 'dummy' code", () => {
-    it('success when compiled and types are checked from declaration', async () => {
+    it.skip('success when compiled and types are checked from onchain descriptor', async () => {
+      const [signer] = await ethers.getSigners();
+
       const lacSourcePath = toLacSourcePath(stubCode, SOURCES_PATH);
-      const compiler = await LacLangCompiler.fromFile(lacSourcePath);
+      const compiler = await LacLangCompiler.fromFile(lacSourcePath, {
+        checkTypesAgainstOnchainDescriptors: true,
+        provider: signer,
+      });
 
       const compilationResult = await compiler.compile();
       const expectedResult = await importJson(
