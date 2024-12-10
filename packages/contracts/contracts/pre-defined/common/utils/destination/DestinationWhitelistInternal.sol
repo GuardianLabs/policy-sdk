95c6c28baa0ee9d00d693763f7d65d8d34a012c7
Vitali Grabovski <vetal2060@gmail.com>
2024-12-11 01:53:52 +0200

    @guardian-network/policy-contracts: Whitelist artifact

---
+++ packages/contracts/contracts/pre-defined/common/utils/destination/DestinationWhitelistInternal.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.27;
+
+import { RECEIVER_ADDRESS_IS_ZERO_ERR } from "./constants/Errors.sol";
+import { AddressDestination } from "./types/Types.sol";
+
+contract DestinationWhitelistInternal {
+    mapping(address receiver => AddressDestination destinationRule) internal destinations;
+
+    function configureDestination(address receiverAdress) internal virtual {
+        destinations[receiverAdress] = AddressDestination({
+            isKnown: true,
+            destination: receiverAdress,
+            allowed: true
+        });
+    }
+
+    function _initializeDestinationsMapping(address[] memory list) internal {
+        for (uint256 i = 0; i < list.length; i++) {
+            address receiver = list[i];
+
+            require(receiver != address(0), RECEIVER_ADDRESS_IS_ZERO_ERR);
+
+            configureDestination(receiver);
+        }
+    }
+
+    function checkDestination(address receiverAddress) internal view virtual returns (bool result) {
+        result = destinations[receiverAddress].allowed == true;
+    }
+}
---
+++ packages/contracts/contracts/pre-defined/common/utils/destination/types/Types.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.27;
+
+struct AddressDestination {
+    bool isKnown;
+    bool allowed;
+    address destination;
+}
