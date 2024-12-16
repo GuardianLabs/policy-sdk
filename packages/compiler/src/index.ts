f238b937f044cfc3ca918fab18f3ec0b34bea07e
Vitali Grabovski <vetal2060@gmail.com>
2024-12-16 13:06:08 +0200

    @guardian-network/policy-compiler: remove redundant codebase

--- packages/compiler/src/index.ts
+++ packages/compiler/src/index.ts
@@ -1,2 +1,1 @@
-// note: using VerboseLacLangCompiler everywhere and keeping less need in the codebase adjustments
-export { VerboseLacLangCompiler as LacLangCompiler } from './VerboseLacLangCompiler';
+export { LacLangCompiler } from './LacLangCompiler';
