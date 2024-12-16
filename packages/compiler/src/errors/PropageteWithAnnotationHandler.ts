80f03f316e53c823fb29f67da89f8af8170e20f3
Vitali Grabovski <vetal2060@gmail.com>
2024-12-16 13:05:58 +0200

    @guardian-network/policy-compiler: add 'propagateWithAnnotation' decorator factory (step b)

--- packages/compiler/src/errors/PropageteWithAnnotationHandler.ts
+++ packages/compiler/src/errors/PropageteWithAnnotationHandler.ts
@@ -1,14 +1,16 @@
+export const execOrPropagate = async <K>(
+  method: () => K | Promise<K>,
+  annotation: string,
+): Promise<Awaited<K>> => {
+  try {
+    const result = await method();
+    return result;
+  } catch (e) {
+    console.error(annotation);
+    throw e;
+  }
+};
+
 export class PropageteWithAnnotationHandler {
-  static execOrPropagate = async <K>(
-    method: () => K | Promise<K>,
-    annotation: string,
-  ): Promise<Awaited<K>> => {
-    try {
-      const result = await method();
-      return result;
-    } catch (e) {
-      console.error(annotation);
-      throw e;
-    }
-  };
+  static execOrPropagate = execOrPropagate;
 }
