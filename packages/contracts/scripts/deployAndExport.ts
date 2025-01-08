aef58b02083562e9239c1890d0bb28f7691564c9
Vitali Grabovski <vetal2060@gmail.com>
2025-01-08 15:49:34 +0200

    @guardian-network/policy-contracts: add script to export pre-defined  artifacts after theri deployment (strep b)

---
+++ packages/contracts/scripts/deployAndExport.ts
@@ -0,0 +1,26 @@
+import hre from 'hardhat';
+import PredefinedArtifactsModule from '../ignition/modules/predefined/deploy-artifacts';
+import { exportAllAtAnyNetwork } from './tools';
+
+const deployIgnitionModule = async () => {
+  const deployments = await hre.ignition.deploy(PredefinedArtifactsModule);
+
+  const deploymentsStringified: string[] = [];
+
+  for (const name of Object.keys(deployments)) {
+    const instance = deployments[name];
+    const stringified = `\n${name}: ${await instance.getAddress()}`;
+    deploymentsStringified.push(stringified);
+  }
+
+  console.log(
+    `The deploy of module "${PredefinedArtifactsModule.id}" is ready. The following contracts are deployed:\n${deploymentsStringified}`,
+  );
+};
+
+const main = async () => {
+  await deployIgnitionModule();
+  await exportAllAtAnyNetwork();
+};
+
+main().catch(console.error);
