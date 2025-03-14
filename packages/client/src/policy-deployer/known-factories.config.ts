e958fafba88f71fd484b4f29f3e9a2f215d89927
Vitali Grabovski <vetal2060@gmail.com>
2025-03-14 13:19:20 +0200

    @guardian-network/policy-client: hardcode policy-deployer addresses for redbdelly, amoy (polygon-testnet), base-mainnet

--- packages/client/src/policy-deployer/known-factories.config.ts
+++ packages/client/src/policy-deployer/known-factories.config.ts
@@ -1,7 +1,15 @@
 import { KnownPolicyFactoriesType } from '../types';
 
-// todo: add polygon, base as hardcoded addresses
-const knownPolicyFactories: KnownPolicyFactoriesType = {};
+// todo: add polygon-mainnet, base-sepolia as hardcoded addresses
+const knownPolicyFactories: KnownPolicyFactoriesType = {
+  [SupportedNetworkIdEnum.amoy]: '0x60854C6cCF5a30c55CaD9ac1323D5499EC1fAFa1',
+  [SupportedNetworkIdEnum.baseMainnet]:
+    '0x7eD5C34B32F5BC9DDB77c5826B1Ce7d96318EdDe',
+  [SupportedNetworkIdEnum.redbellyMainnet]:
+    '0x2AeFca4dab98c8DA4337C615DCA1D94cc6477e5c',
+  [SupportedNetworkIdEnum.redbellyTestnet]:
+    '0xE1f341924Cd168DfE9Eac2B5b3ca9bc7AD57D325',
+};
 
 export const getKnownPolicyFactories = (): KnownPolicyFactoriesType => {
   return structuredClone(knownPolicyFactories);
